4f7160cca5ab044b977d71b159c7de188617a333
RykhtaYaroslav <iupacmanshop@yandex.ru>
2026-09-04 20:30:16 +0700

    refactor: Add hub event handling to collector controller (2-collector-grpc)
    Introduces handling

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/controller/CollectorController.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/controller/CollectorController.java
@@ -7,20 +7,23 @@ import io.grpc.stub.StreamObserver;
 import lombok.RequiredArgsConstructor;
 import net.devh.boot.grpc.server.service.GrpcService;
 import ru.yandex.practicum.grpc.telemetry.collector.CollectorControllerGrpc;
+import ru.yandex.practicum.grpc.telemetry.event.HubEventProto;
 import ru.yandex.practicum.grpc.telemetry.event.SensorEventProto;
-import ru.yandex.practicum.telemetry.collector.handler.SensorEventHandler;
+import ru.yandex.practicum.telemetry.collector.handler.event.SensorEventHandler;
+import ru.yandex.practicum.telemetry.collector.handler.hub.HubEventHandler;
 
 import java.util.Map;
 
 @GrpcService
 @RequiredArgsConstructor
 public class CollectorController extends CollectorControllerGrpc.CollectorControllerImplBase {
-    private final Map<SensorEventProto.PayloadCase, SensorEventHandler> sensorEventHandlerMap;
+    private final Map<SensorEventProto.PayloadCase, SensorEventHandler> sensorHandlers;
+    private final Map<HubEventProto.PayloadCase, HubEventHandler> hubHandlers;
 
     @Override
     public void collectSensorEvent(SensorEventProto request, StreamObserver<Empty> responseObserver) {
         try {
-            SensorEventHandler handler = sensorEventHandlerMap.get(request.getPayloadCase());
+            SensorEventHandler handler = sensorHandlers.get(request.getPayloadCase());
 
             if (handler == null) {
                 String m = String.format("Unknown type of sensor event: %s", request.getPayloadCase());
@@ -39,4 +42,28 @@ public class CollectorController extends CollectorControllerGrpc.CollectorContro
             ));
         }
     }
+
+    @Override
+    public void collectHubEvent(HubEventProto request, StreamObserver<Empty> responseObserver) {
+        try {
+            HubEventHandler handler = hubHandlers.get(request.getPayloadCase());
+
+            if (handler == null) {
+                String m = String.format("Unknown type of hub event: %s", request.getPayloadCase());
+                throw new IllegalArgumentException(m);
+            }
+
+            handler.handle(request);
+
+            responseObserver.onNext(Empty.getDefaultInstance());
+            responseObserver.onCompleted();
+        } catch (Exception e) {
+            responseObserver.onError(new StatusRuntimeException(
+                    Status.INTERNAL
+                            .withDescription(e.getLocalizedMessage())
+                            .withCause(e)
+            ));
+        }
+    }
+
 }
