0ae48be6f6f9b4fedce8a186e5eaf20ad56b6782
RykhtaYaroslav <iupacmanshop@yandex.ru>
2026-09-07 14:18:19 +0700

    feat(aggregator): Implement sensor state update logic (3)

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/telemetry/aggregator/AggregationServiceImpl.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/telemetry/aggregator/AggregationServiceImpl.java
@@ -2,14 +2,50 @@ package ru.yandex.practicum.telemetry.aggregator;
 
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
+import ru.yandex.practicum.kafka.telemetry.event.SensorStateAvro;
 import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
 public class AggregationServiceImpl implements AggregationService {
+    private final Map<String, SensorsSnapshotAvro> snapshots = new HashMap<>();
+
     @Override
     public Optional<SensorsSnapshotAvro> updateState(SensorEventAvro event) {
-        return Optional.empty();
+        SensorsSnapshotAvro snapshot = snapshots.computeIfAbsent(
+                event.getHubId(),
+                hubId -> SensorsSnapshotAvro.newBuilder()
+                        .setHubId(hubId)
+                        .setTimestamp(event.getTimestamp())
+                        .setSensorsState(new HashMap<>())
+                        .build()
+        );
+
+        SensorStateAvro oldState = snapshot.getSensorsState().get(event.getId());
+
+        if (oldState != null) {
+            boolean eventIsOlder = oldState.getTimestamp().isAfter(event.getTimestamp());
+
+            boolean dataDidNotChange = oldState.getData().equals(event.getPayload());
+
+            if (eventIsOlder || dataDidNotChange) {
+                return Optional.empty();
+            }
+        }
+
+        SensorStateAvro newState = SensorStateAvro.newBuilder()
+                .setTimestamp(event.getTimestamp())
+                .setData(event.getPayload())
+                .build();
+
+        snapshot.getSensorsState()
+                .put(event.getId(), newState);
+
+        snapshot.setTimestamp(event.getTimestamp());
+
+        return Optional.of(snapshot);
     }
 }
