a386d86407d9350326117548903ae389ea3388c0
RykhtaYaroslav <iupacmanshop@yandex.ru>
2026-09-08 21:24:36 +0700

    feat(analyzer): Implement HubEventService for handling events

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/service/HubEventService.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/service/HubEventService.java
@@ -1,4 +1,7 @@
 package ru.yandex.practicum.telemetry.analyzer.service;
 
+import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
+
 public interface HubEventService {
+    void handle(HubEventAvro hubEventAvro);
 }
--- telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/service/HubEventServiceImpl.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/service/HubEventServiceImpl.java
@@ -1,4 +1,34 @@
 package ru.yandex.practicum.telemetry.analyzer.service;
 
-public class HubEventServiceImpl {
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Service
+public class HubEventServiceImpl implements HubEventService {
+    private final Map<Class<?>, HubEventHandler> handlers;
+
+    public HubEventServiceImpl(Set<HubEventHandler> handlers) {
+        this.handlers = handlers.stream().collect(Collectors.toUnmodifiableMap(
+                HubEventHandler::getPayloadType,
+                Function.identity()
+        ));
+    }
+
+    @Override
+    public void handle(HubEventAvro hubEventAvro) {
+        Class<?> clazz = hubEventAvro.getPayload().getClass();
+
+        HubEventHandler handler = handlers.get(clazz);
+
+        if (handler == null) {
+            String m = "Неверный тип события: " + clazz.getName();
+            throw new IllegalArgumentException(m);
+        }
+        handler.handle(hubEventAvro);
+    }
 }
