a37bdd550bd73ac3e740b8b9e8814aacef1f10de
RykhtaYaroslav <iupacmanshop@yandex.ru>
2026-09-19 04:26:06 +0700

    feat: Implement custom exception handling for product service (7)

---
+++ commerce/product-service/src/main/java/ru/yandex/practicum/product/exception/BaseCustomException.java
@@ -0,0 +1,17 @@
+package ru.yandex.practicum.product.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class BaseCustomException extends RuntimeException {
+    private final HttpStatus status;
+    private final String description;
+
+    public BaseCustomException(String message, String description, HttpStatus status) {
+        super(message);
+        this.status = status;
+        this.description = description;
+    }
+
+}
